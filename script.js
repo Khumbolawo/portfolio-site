05430e192a7605aa0a6826cf67544b532aece063
Khumbolawo <khumbo87@gmail.com>
2024-03-12 20:45:31 +0200

    Tried and failed to add dark mode

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+const darkModeToggle = document.getElementById('darkModeToggle');
+
+darkModeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  
+  // save preference to localStorage
+  if(document.body.classList.contains('dark-mode')){
+    localStorage.setItem('darkMode', 'enabled');
+  } else {
+    localStorage.setItem('darkMode', 'disabled');
+  }
+});
+
+// check local storage for dark mode preference
+const darkMode = localStorage.getItem('darkMode');
+if(darkMode === 'enabled'){
+  document.body.classList.add('dark-mode');
+}
